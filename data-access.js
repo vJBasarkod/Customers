216c4ece94f2cd6e59a481a9f1d6992da5fe587f
VJ <basarkod@gmail.com>
2025-07-16 15:29:13 -0400

    vj repo

---
+++ data-access.js
@@ -0,0 +1,102 @@
+const MongoClient = require('mongodb').MongoClient;
+const dbName = 'custdb';
+const baseUrl = "mongodb://127.0.0.1:27017";
+const collectionName = "customers"
+const connectString = baseUrl + "/" + dbName; 
+let collection;
+
+async function dbStartup() {
+    const client = new MongoClient(connectString);
+    await client.connect();
+    collection = client.db(dbName).collection(collectionName);
+}
+
+async function getCustomers() {
+    try {
+        const customers = await collection.find().toArray();
+        //throw {"message":"an error occured"};
+        return [customers, null];
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
+}
+
+async function getCustomerById(id) {
+    try {
+        const customer = await collection.findOne({"id": +id});
+        // return array [customer, errMessage]
+        if(!customer){
+          return [ null, "invalid customer number"];
+        }
+        return [customer, null];
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
+}
+
+async function addCustomer(newCustomer) {
+    try {
+        const insertResult = await collection.insertOne(newCustomer);
+        // return array [status, id, errMessage]
+        return ["success", insertResult.insertedId, null];
+    } catch (err) {
+        console.log(err.message);
+        return ["fail", null, err.message];
+    }
+}
+
+
+async function resetCustomers() {
+    let data = [{ "id": 0, "name": "Mary Jackson", "email": "maryj@abc.com", "password": "maryj" },
+    { "id": 1, "name": "Karen Addams", "email": "karena@abc.com", "password": "karena" },
+    { "id": 2, "name": "Scott Ramsey", "email": "scottr@abc.com", "password": "scottr" }];
+
+    try {
+        await collection.deleteMany({});
+        await collection.insertMany(data);
+        const customers = await collection.find().toArray();
+        const message = "data was refreshed. There are now " + customers.length + " customer records!"
+        return [message, null];
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
+}
+
+async function updateCustomer(updatedCustomer) {
+    try {
+        const filter = { "id": updatedCustomer.id };
+        const setData = { $set: updatedCustomer };
+        const updateResult = 
+        await collection.updateOne(filter, setData);
+        // return array [message, errMessage]
+        return ["one record updated", null];
+    } catch (err) {
+        console.log(err.message);
+        return [ null, err.message];
+    }
+}
+
+async function deleteCustomerById(id) {
+    try {
+        const deleteResult = await collection.deleteOne({ "id": +id });
+        if (deleteResult.deletedCount === 0) {
+            // return array [message, errMessage]
+            return [null, "no record deleted"];
+        } else if (deleteResult.deletedCount === 1) {
+            return ["one record deleted", null];
+        } else {
+            return [null, "error deleting records"]
+        }
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
+}
+
+
+dbStartup();
+module.exports = { getCustomers, resetCustomers, addCustomer, 
+  getCustomerById, updateCustomer, deleteCustomerById };
---
+++ server.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const da = require("./data-access");
+const path = require('path'); 
+
+const app = express();
+const port = process.env.PORT || 4000;  // use env var or default to 4000
+
+app.use(bodyParser.json());
+
+// Set the static directory to serve files from
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+  console.log("staticDir: " + staticDir);
+});
+
+app.get("/customers", async (req, res) => {
+    const [cust, err] = await da.getCustomers();
+    if(cust){
+        res.send(cust);
+    }else{
+        res.status(500);
+        res.send(err);
+    }   
+});
+
+app.get("/customers/:id", async (req, res) => {
+    const id = req.params.id;
+    const [cust, err] = await da.getCustomerById(id);
+    if(cust){
+        res.send(cust);
+    }else{
+        res.status(404);
+        res.send(err);
+    }   
+});
+
+app.get("/reset", async (req, res) => {
+    const [result, err] = await da.resetCustomers();
+    if(result){
+        res.send(result);
+    }else{
+        res.status(500);
+        res.send(err);
+    }   
+});
+
+app.post('/customers', async (req, res) => {
+    const newCustomer = req.body;
+    if (newCustomer === null) {
+        res.status(400);
+        res.send("missing request body");
+    } else {
+        // return array format [status, id, errMessage]
+        const [status, id, errMessage] = await da.addCustomer(newCustomer);
+        if (status === "success") {
+            res.status(201);
+            let response = { ...newCustomer };
+            response["_id"] = id;
+            res.send(response);
+        } else {
+            res.status(400);
+            res.send(errMessage);
+        }
+    }
+});
+
+app.put('/customers/:id', async (req, res) => {
+    const id = req.params.id;
+    const updatedCustomer = req.body;
+    if (updatedCustomer === null) {
+        res.status(400);
+        res.send("missing request body");
+    } else {
+        delete updatedCustomer._id;
+        // return array format [message, errMessage]
+        const [message, errMessage] = await da.updateCustomer(updatedCustomer);
+        if (message) {
+            res.send(message);
+        } else {
+            res.status(400);
+            res.send(errMessage);
+        }
+    }
+});
+
+app.delete("/customers/:id", async (req, res) => {
+    const id = req.params.id;
+    // return array [message, errMessage]
+    const [message, errMessage] = await da.deleteCustomerById(id);
+    if (message) {
+        res.send(message);
+    } else {
+        res.status(404);
+        res.send(errMessage);
+    }
+});
+
