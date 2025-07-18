0bd401432ddd1ab405b480ba0645bd5a4ec18ba4
VJ <basarkod@gmail.com>
2025-07-18 14:16:05 -0400

    commit

--- data-access.js
+++ data-access.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const dbName = 'custdb';
-const baseUrl = 'mongodb://localhost:27017';
-const collectionName = "customers";
-const connectString = baseUrl + '/' + dbName;
+const baseUrl = "mongodb://127.0.0.1:27017";
+const collectionName = "customers"
+const connectString = baseUrl + "/" + dbName; 
 let collection;
 
 async function dbStartup() {
@@ -12,42 +12,31 @@ async function dbStartup() {
 }
 
 async function getCustomers() {
-     try {
-         const customers = await collection.find().toArray();
-         return [customers, null];
-     } catch (err) {
-         console.log(err.message);
-         return [null, err.message];
-     }
-}
-
-async function resetCustomers() {
-    let data = [{ "id": 0, "name": "Mary Jackson", "email": "maryj@abc.com", "password": "maryj" },
-    { "id": 1, "name": "Karen Addams", "email": "karena@abc.com", "password": "karena" },
-    { "id": 2, "name": "Scott Ramsey", "email": "scottr@abc.com", "password": "scottr" }];
-
     try {
-        await collection.deleteMany({});
-        await collection.insertMany(data);
         const customers = await collection.find().toArray();
-        const message = "data was refreshed. There are now " + customers.length + " customer records!"
-        return [message, null];
+        //throw {"message":"an error occured"};
+        return [customers, null];
     } catch (err) {
         console.log(err.message);
         return [null, err.message];
     }
 }
 
-async function addCustomer(newCustomer) {
+
+async function getCustomerByEmail(email) {
     try {
-        const insertResult = await collection.insertOne(newCustomer);
-        // return array [status, id, errMessage]
-        return ["success", insertResult.insertedId, null];
+        const customer = await collection.findOne({"email": email});
+        // return array [customer, errMessage]
+        if(!customer){
+          return [null, "email not used"];
+        }
+        return [customer, null];
     } catch (err) {
         console.log(err.message);
-        return ["fail", null, err.message];
+        return [null, err.message];
     }
-}
+}   
+
 
 async function getCustomerById(id) {
     try {
@@ -99,12 +88,41 @@ async function findCustomer(key, value) {
     }
 }
 
+async function addCustomer(newCustomer) {
+    try {
+        const insertResult = await collection.insertOne(newCustomer);
+        // return array [status, id, errMessage]
+        return ["success", insertResult.insertedId, null];
+    } catch (err) {
+        console.log(err.message);
+        return ["fail", null, err.message];
+    }
+}
+
+
+async function resetCustomers() {
+    let data = [{ "id": 0, "name": "Mary Jackson", "email": "maryj@abc.com", "password": "maryj" },
+    { "id": 1, "name": "Karen Addams", "email": "karena@abc.com", "password": "karena" },
+    { "id": 2, "name": "Scott Ramsey", "email": "scottr@abc.com", "password": "scottr" }];
+
+    try {
+        await collection.deleteMany({});
+        await collection.insertMany(data);
+        const customers = await collection.find().toArray();
+        const message = "data was refreshed. There are now " + customers.length + " customer records!"
+        return [message, null];
+    } catch (err) {
+        console.log(err.message);
+        return [null, err.message];
+    }
+}
+
 async function updateCustomer(updatedCustomer) {
     try {
         const filter = { "id": updatedCustomer.id };
         const setData = { $set: updatedCustomer };
-        const option = { upsert: true }; // insert if not found
-        const updateResult = await collection.updateOne(filter, setData, option);
+        const updateResult = 
+        await collection.updateOne(filter, setData);
         // return array [message, errMessage]
         return ["one record updated", null];
     } catch (err) {
@@ -130,17 +148,7 @@ async function deleteCustomerById(id) {
     }
 }
 
-dbStartup()
-    .then(() => console.log("Database connection established"))
-    .catch(err => console.error("Database connection failed:", err));
 
-module.exports = {
-    getCustomers,
-    resetCustomers,
-    addCustomer,
-    getCustomerById,
-    updateCustomer,
-    deleteCustomerById,
-    findCustomer,
-    getCustomerByEmail
-};
+dbStartup();
+module.exports = { getCustomers, resetCustomers, addCustomer, 
+  getCustomerById, updateCustomer, deleteCustomerById };
